86856a1adab8ec038bf8ff996135a8942a5dfb23
hadivafaii <vafaii@berkeley.edu>
2025-02-05 15:37:10 -0800

    moved distributions.py to main

--- main/distributions.py
+++ main/distributions.py
@@ -34,23 +34,15 @@ class Poisson:
 	def variance(self):
 		return self.rate
 
-	def rsample(self, hard: bool = False):
+	def rsample(self):
+		if self.temp == 0:
+			return self.sample()
 		# noinspection PyTypeChecker
 		x = self.exp.rsample((self.n_exp,))  # inter-event times
-		times = torch.cumsum(x, dim=0)  # arrival times of events
-
-		indicator = times < 1.0
-		z_hard = indicator.sum(0).float()
-
-		if self.temp > 0:
-			indicator = torch.sigmoid(
-				(1.0 - times) / self.temp)
-			z = indicator.sum(0).float()
-		else:
-			z = z_hard
-
-		if hard:
-			return z + (z_hard - z).detach()
+		times = torch.cumsum(x, dim=0)       # arrival times of events
+		indicator = torch.sigmoid(           # events within [0, 1]
+			(1.0 - times) / self.temp)
+		z = indicator.sum(0).float()         # event counts
 		return z
 
 	def sample(self):
@@ -62,140 +54,6 @@ class Poisson:
 			- torch.lgamma(samples + 1)
 			+ samples * torch.log(self.rate)
 		)
-
-
-class PoissonQuantile:
-	def __init__(
-			self,
-			log_rate: torch.Tensor,
-			temp: float = 0.0,
-			q: List[float] = None,
-			clamp: float = 5.3,
-	):
-		assert temp >= 0.0, f"must be non-neg: {temp}"
-		self.t = temp
-		self.c = clamp
-		self._init_rates(log_rate)
-		self._init_quantiles(q)
-		self._init_masks()
-		self._init_n_exp()
-
-	@property
-	def mean(self):
-		return self.rate
-
-	@property
-	def variance(self):
-		return self.rate
-
-	def rsample(self, hard: bool = False):
-		samples = torch.zeros_like(
-			self.rate.ravel())
-		index = torch.zeros(
-			self.rate.numel(),
-			dtype=torch.long,
-			device=self.rate.device,
-		)
-		z = []
-		offset = 0
-		for i, m in self.masks.items():
-			z.append(self._rsample_per_group(
-				rates=self.rate.ravel()[m],
-				n_exp=self.n_exp[i],
-				hard=hard,
-			))
-			index[m] = torch.arange(
-				int(m.sum()),
-				device=m.device,
-			) + offset
-			offset += m.sum()
-		samples.scatter_(0, index, torch.cat(z))
-		samples = samples.view(self.rate.shape)
-		return samples
-
-	# noinspection PyTypeChecker
-	def _rsample_per_group(
-			self,
-			rates: torch.Tensor,
-			n_exp: int,
-			hard: bool = False, ):
-		exp = dists.Exponential(rates)
-		x = exp.rsample((n_exp,))
-		times = torch.cumsum(x, dim=0)
-
-		indicator = times < 1.0
-		z_hard = indicator.sum(0).float()
-
-		if self.t > 0:
-			indicator = torch.sigmoid(
-				(1.0 - times) / self.t)
-			z = indicator.sum(0).float()
-		else:
-			z = z_hard
-
-		if hard:
-			return z + (z_hard - z).detach()
-		return z
-
-	def sample(self):
-		return torch.poisson(self.rate).float()
-
-	def log_p(self, samples: torch.Tensor):
-		return (
-			- self.rate
-			- torch.lgamma(samples + 1)
-			+ samples * torch.log(self.rate)
-		)
-
-	def _init_rates(self, log_rate):
-		eps = torch.finfo(torch.float32).eps
-		log_rate = softclamp_upper(log_rate, self.c)
-		self.rate = torch.exp(log_rate) + eps
-		return
-
-	@torch.no_grad()
-	def _init_quantiles(self, quantiles):
-		quantiles = quantiles or [0.5, 0.75, 0.95]
-		self.q = sorted(set(quantiles))
-		return
-
-	@torch.no_grad()
-	def _init_masks(self):
-		masks = {}
-		rate_flat = self.rate.ravel()
-		for i in range(len(self.q) + 1):
-			if i == 0:
-				higher = torch.quantile(
-					rate_flat, self.q[i])
-				m = rate_flat < higher
-			elif i == len(self.q):
-				lower = torch.quantile(
-					rate_flat, self.q[i - 1])
-				m = rate_flat >= lower
-			else:
-				lower = torch.quantile(
-					rate_flat, self.q[i - 1])
-				higher = torch.quantile(
-					rate_flat, self.q[i])
-				m = (
-					(lower <= rate_flat) &
-					(rate_flat < higher)
-				)
-			masks[i] = m
-		self.masks = masks
-		return
-
-	@torch.no_grad()
-	def _init_n_exp(self):
-		r_max = {
-			i: self.rate.ravel()[m].max().item()
-			for i, m in self.masks.items()
-		}
-		self.n_exp = {
-			i: compute_n_exp(r, 1e-6)
-			for i, r in r_max.items()
-		}
-		return
 
 
 # noinspection PyAbstractClass
@@ -376,74 +234,11 @@ class PoissonTorch(dists.Poisson):
 		)
 
 
-class GaBe:
-	def __init__(
-			self,
-			mu: torch.Tensor,
-			logsig: torch.Tensor,
-			temp: float = 1.0,
-			clamp: float = 5.3,
-			seed: int = None,
-			device: torch.device = None,
-	):
-		self.mu = mu
-		logsig = softclamp_sym(logsig, clamp)
-		self.sigma = torch.exp(logsig)
-		assert temp >= 0
-		self.temp = temp
-		if temp != 1.0:
-			self.sigma *= temp
-		if seed is not None:
-			self.rng = torch.Generator(device)
-			self.rng.manual_seed(seed)
-		else:
-			self.rng = None
-
-	def rsample(self):
-		if self.rng is None:
-			u = sample_normal_jit(self.mu, self.sigma)
-		else:
-			u = sample_normal(self.mu, self.sigma, self.rng)
-		p = phi(u)
-		# TODO: z = sample_bernoulli(p)
-		#  return z
-		return
-
-	def log_p(self, samples: torch.Tensor):
-		zscored = (samples - self.mu) / self.sigma
-		log_p = (
-			- 0.5 * zscored.pow(2)
-			- 0.5 * np.log(2*np.pi)
-			- torch.log(self.sigma)
-		)
-		return log_p
-
-	def kl(self, p=None):
-		if p is None:
-			term1 = self.mu
-			term2 = self.sigma
-		else:
-			term1 = (self.mu - p.mu) / p.sigma
-			term2 = self.sigma / p.sigma
-		kl = 0.5 * (
-			term1.pow(2) + term2.pow(2) +
-			torch.log(term2).mul(-2) - 1
-		)
-		return kl
-
-
 def compute_n_exp(rate: float, p: float = 1e-6):
 	assert rate > 0.0, f"must be positive, got: {rate}"
 	pois = sp_stats.poisson(rate)
 	n_exp = pois.ppf(1.0 - p)
 	return int(n_exp)
-
-
-def phi(x):
-	sqrt2 = torch.sqrt(torch.tensor(2.0))
-	x = torch.special.erf(x.div(sqrt2))
-	x = (1 + x).mul(0.5)
-	return x
 
 
 def softclamp(x: torch.Tensor, upper: float, lower: float = 0.0):
